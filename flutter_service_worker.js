41c5d31595f74cb9a939cfc9460647e95d902d3f
usmanasad87 <usmanasad0324@gmail.com>
2024-05-13 01:32:44 +0500

    13 May Build Web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "7f2194a8c690fa983e51da1c277b8ab1",
 "favicon.ico": "a8ffd274fd4acf213442f74f87b390c9",
-"index.html": "9523544ba1aecec675ad6ff5cac9f168",
-"/": "9523544ba1aecec675ad6ff5cac9f168",
-"main.dart.js": "e4cce3e3e7132c50ab0c05db9e34bdb9",
+"index.html": "60a8f795056b665f0556ace6f664fc4e",
+"/": "60a8f795056b665f0556ace6f664fc4e",
+"main.dart.js": "211da371f4043f36de4a97cd693c68a9",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
