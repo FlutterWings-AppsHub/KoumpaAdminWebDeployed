f613b6606a923a862c35e55d50a403365f945c51
usmanasad87 <usmanasad0324@gmail.com>
2024-05-16 04:55:33 +0500

    Koumpa Web Deploy 16 May

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "7f2194a8c690fa983e51da1c277b8ab1",
 "favicon.ico": "a8ffd274fd4acf213442f74f87b390c9",
-"index.html": "d62bb8a2686f54baa481509b3e98160d",
-"/": "d62bb8a2686f54baa481509b3e98160d",
-"main.dart.js": "d41fb735dc5f0e6bbac83e6d4de0a00c",
+"index.html": "e1a3cee08bd76a1a76c353f7b3d43fca",
+"/": "e1a3cee08bd76a1a76c353f7b3d43fca",
+"main.dart.js": "e3b9059aa3ec4598ee112470f65a595b",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
